16ba53c755b6bfd1a69a8214245f71588f607e17
Chris-Zionna-A <100009578+Chris-Zionna-A@users.noreply.github.com>
2022-09-28 10:00:08 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,14 @@
 from typing import List
 
 def selectionSort(array, size) -> List[int]:
-  # Write your code here
-
+  for i in range(0,size):
+    for j in range (i+1, size):
+      if array[i]>array[j]:
+        temp=array[i]
+        array[i]=array[j]
+        array[j]=temp
+        
+        
 # Do not change the following code
 input_data = input()
 data = []
